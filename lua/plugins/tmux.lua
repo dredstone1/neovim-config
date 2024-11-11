ddfbbf2514dec1c93ea24a892787674617f5fb9f
maayan <mayanpotu500@gmail.com>
2024-11-11 18:04:00 +0200

    test

--- lua/plugins/tmux.lua
+++ lua/plugins/tmux.lua
@@ -3,7 +3,6 @@ return {
     config = function()
         return require("tmux").setup({
             copy_sync = {
-                -- enables copy sync. by default, all registers are synchronized.
                 -- to control which registers are synced, see the `sync_*` options.
                 enable = true,
 
