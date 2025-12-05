75584662217f51a2fb34d6983a912f99423661dd
maayan <mayanpotu500@gmail.com>
2025-12-05 17:13:47 +0200

    new commit

--- lua/plugins/conform.lua
+++ lua/plugins/conform.lua
@@ -27,6 +27,7 @@ return {
 				javascript = { "prettierd", "prettier", stop_after_first = true },
 				cpp = { "clang-format" },
 				c = { "clang-format" },
+				cuda = { "clang-format" },
 				["*"] = { "codespell" },
 				["_"] = { "trim_whitespace" },
 			},
